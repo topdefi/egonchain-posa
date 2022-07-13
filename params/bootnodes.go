9c1a5bd8e844bf1bdfb85df415509fccd78b0829
Topdefi <94203269+topdefi@users.noreply.github.com>
2022-07-12 21:12:35 -0700

    Initial Upload

---
+++ params/bootnodes.go
@@ -0,0 +1,41 @@
+// Copyright 2015 The go-ethereum Authors
+// This file is part of the go-ethereum library.
+//
+// The go-ethereum library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-ethereum library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+
+package params
+
+import "github.com/ethereum/go-ethereum/common"
+
+// MainnetBootnodes are the enode URLs of the P2P bootstrap nodes running on
+// the main EGC network.
+var MainnetBootnodes = []string{
+	"",  // egc-mainnet-node-boot-01
+	"",  //egc-mainnet-node-boot-02
+}
+
+// TestnetBootnodes are the enode URLs of the P2P bootstrap nodes running on the test network.
+var TestnetBootnodes = []string{
+	"", // egc-testnet-node-boot-01
+	"",  // egc-testnet-node-boot-02
+}
+
+var V5Bootnodes []string
+
+// KnownDNSNetwork returns the address of a public DNS-based node list for the given
+// genesis hash and protocol. See https://github.com/ethereum/discv4-dns-lists for more
+// information.
+func KnownDNSNetwork(genesis common.Hash, protocol string) string {
+	return ""
+}
